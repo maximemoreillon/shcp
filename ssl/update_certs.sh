e3e89c69bce8e8ce69df2849ea21b93359e745c8
Maxime Moreillon <m.moreillon@gmail.com>
2018-09-28 10:56:08 +0900

    Delete update_certs.sh

--- ssl/update_certs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-cp /etc/letsencrypt/live/moreillon.duckdns.org/fullchain.pem fullchain.pem
-cp /etc/letsencrypt/live/moreillon.duckdns.org/privkey.pem privkey.pem
-
-chown moreillon:moreillon fullchain.pem
-chown moreillon:moreillon privkey.pem
