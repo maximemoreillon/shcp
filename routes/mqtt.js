c8d223723af3284aaf0519b1ef0f4f9378b2ae99
Maxime Moreillon <m.moreillon@gmail.com>
2023-01-04 18:39:14 +0900

    update of mqtt package

--- routes/mqtt.js
+++ routes/mqtt.js
@@ -5,7 +5,7 @@ const router = Router()
 router.post("/", (req, res, next) => {
   const { topic, payload } = req.body
 
-  console.log(`[WS to MQTT] Publishing to ${topic}`)
+  console.log(`[MQTT] Publishing to ${topic}`)
 
   const publish_options = { qos: 1, retain: true }
 
